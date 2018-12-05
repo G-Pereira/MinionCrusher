b4c3897f8d726f356e4688067244047f62167073
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2018-12-05 18:05:52 +0100

    Added function to measure distance

--- include/types.h
+++ include/types.h
@@ -9,12 +9,18 @@
 #ifndef MINIONCRUSHER_TYPES_H
 #define MINIONCRUSHER_TYPES_H
 
+#include <cmath>
+
 /**
  * Structure holding the X, Y coordinates in a cartesian frame.
  */
 struct CartesianCoordinates {
     float x;
     float y;
+
+    bool isInRange(const CartesianCoordinates target, float range) {
+        return (sqrt(pow(target.x - x, 2) + pow(target.y - y, 2)) < range);
+    }
 };
 
 /**
@@ -30,11 +36,11 @@ struct ObjectSize {
  * Structure holding all configuration parameters that influence the behavior of an ammo related type.
  */
 struct AmmoType {
-    float speed=1;
-    float damage=1;
-    float blastRadius=0;
-    float pierce=0;
-    float slow=0;
+    float speed = 1;
+    float damage = 1;
+    float blastRadius = 0;
+    float pierce = 0;
+    float slow = 0;
 };
 
 /**
