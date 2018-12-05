27c031ddbf3565d3fbc0e96b455ca7660b4cbc0a
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2018-12-05 17:51:05 +0100

    Missing switch handle

--- src/TileView.cpp
+++ src/TileView.cpp
@@ -1,31 +1,30 @@
 #include "TileView.h"
 
-TileView::TileView(SDL_Rect quad, MapSlots type, UIView * parent):UIView(quad, parent), type(type)
-{
+TileView::TileView(SDL_Rect quad, MapSlots type, UIView *parent) : UIView(quad, parent), type(type) {
 }
 
 
-TileView::~TileView()
-{
+TileView::~TileView() {
 }
 
-void TileView::postRender()
-{
-	//Render texture to screen
-	SDL_RendererFlip flip = (SDL_RendererFlip)0;
-	double angle = 0.0;
-	switch (type) {
-	case (MapSlots)1:
-		flip = (SDL_RendererFlip)1;
-		break;
-	case (MapSlots)2:
-		angle = 90.0;
-		break;
-	case (MapSlots)3:
-		angle = -90.0;
-		break;
-	case (MapSlots)4:
-		break;
-	}
-	SDL_RenderCopyEx(renderer, background, nullptr, &quad, angle, nullptr, flip);
+void TileView::postRender() {
+    //Render texture to screen
+    SDL_RendererFlip flip = (SDL_RendererFlip) 0;
+    double angle = 0.0;
+    switch (type) {
+        case (MapSlots) 0:
+            break;
+        case (MapSlots) 1:
+            flip = (SDL_RendererFlip) 1;
+            break;
+        case (MapSlots) 2:
+            angle = 90.0;
+            break;
+        case (MapSlots) 3:
+            angle = -90.0;
+            break;
+        case (MapSlots) 4:
+            break;
+    }
+    SDL_RenderCopyEx(renderer, background, nullptr, &quad, angle, nullptr, flip);
 }
