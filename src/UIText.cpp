ab89de3fa2a8ecd8a72e51814a9e675a2f0e9c54
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2018-12-17 21:34:56 +0100

    Set correct text

--- src/UIText.cpp
+++ src/UIText.cpp
@@ -9,7 +9,7 @@ UIText::UIText(SDL_Rect quad, std::string text, UIElement *parent) :
 
     SDL_Color White = {255, 255, 255};  // this is the color in rgb format, maxing out all would give you the color white, and it will be your text's color
 
-    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, "put your text here", White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
+    SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans, text.c_str(), White); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
 
     SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage); //now you can convert it into a texture TODO: fix renderer
 }
