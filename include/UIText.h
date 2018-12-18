9f9f86d82302aaec05d587974750976b5cccb72f
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2018-12-18 02:28:38 +0100

    Commented UIText

--- include/UIText.h
+++ include/UIText.h
@@ -1,3 +1,9 @@
+/**
+ * Author: Gonçalo Pereira
+ * Created on: 10-12-18
+ * Last Modified: 17-12-18
+ */
+
 #ifndef MINIONCRUSHER_TEXTFIELD_H
 #define MINIONCRUSHER_TEXTFIELD_H
 
@@ -6,6 +12,12 @@
 
 class UIText : public UIElement {
 public:
+
+    /**
+    * \brief Element that spawns a text element in the User interface
+    * @param: quad Text field boundaries and position
+    * @param: parent Entity that holds this User Interface Element
+    */
     UIText(SDL_Rect quad, UIElement *parent);
 
     ~UIText();
