a1077e30ba718f480a31b1936fbb1330756c1e05
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2019-01-12 18:37:48 +0100

    Played around with different fonts

--- src/UIText.cpp
+++ src/UIText.cpp
@@ -13,7 +13,7 @@ void UIText::postRender() {
         case info_types::health:
             if (prev_value != gamemanager->map->base.getHealth()) {
                 std::string disp = "health: " + std::to_string((int) gamemanager->map->base.getHealth());
-                quad.w = (int) disp.size()*quad.h/3;
+                quad.w = (int) disp.size()*quad.h/2;
                 loadText2Texture(disp);
                 prev_value = gamemanager->map->base.getHealth();
             }
@@ -21,7 +21,7 @@ void UIText::postRender() {
         case info_types::kills:
             if (prev_value != gamemanager->kills) {
                 std::string disp = "kills: " + std::to_string(gamemanager->kills);
-                quad.w = (int) disp.size()*quad.h/3;
+                quad.w = (int) disp.size()*quad.h/2;
                 loadText2Texture(disp);
                 prev_value = gamemanager->kills;
             }
@@ -29,7 +29,7 @@ void UIText::postRender() {
         case info_types::money:
             if (prev_value != gamemanager->money) {
                 std::string disp = "money: " + std::to_string(gamemanager->money);
-                quad.w = (int) disp.size()*quad.h/3;
+                quad.w = (int) disp.size()*quad.h/2;
                 loadText2Texture(disp);
                 prev_value = gamemanager->money;
             }
@@ -37,7 +37,7 @@ void UIText::postRender() {
         case info_types::level:
             if (prev_value != gamemanager->level) {
                 std::string disp = "level: " + std::to_string(gamemanager->level);
-                quad.w = (int) disp.size()*quad.h/3;
+                quad.w = (int) disp.size()*quad.h/2;
                 loadText2Texture(disp);
                 prev_value = gamemanager->level;
             }
@@ -45,7 +45,7 @@ void UIText::postRender() {
         case info_types::state:
             if (prev_value != gamemanager->cooldownTime) {
                 std::string disp = "cooldown: " + std::to_string(gamemanager->cooldownTime / 500) + "s";
-                quad.w = (int) disp.size()*quad.h/3;
+                quad.w = (int) disp.size()*quad.h/2;
                 loadText2Texture(disp);
                 prev_value = gamemanager->cooldownTime;
             }
