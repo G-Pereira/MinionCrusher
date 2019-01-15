00ae12f5c54bd455559b13f4fa039a52cc5fbf61
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2019-01-15 23:45:31 +0100

    More Documentation

--- include/BuildView.h
+++ include/BuildView.h
@@ -9,10 +9,18 @@
 class BuildView :
         public UIElement {
 public:
+    /**
+     * Build View
+     * @param parent Container to insert the build view
+     */
     BuildView(SDL_Rect, UIElement *parent);
 
     ~BuildView();
 
+    /**
+     * Add a button to the Build View
+     * @param button_type Type of button
+     */
     void addButton(ButtonTypes button_type);
 
 private:
--- include/gameManager.h
+++ include/gameManager.h
@@ -4,38 +4,112 @@
 #include "Map.h"
 #include "types.h"
 
-
-/** Needs to be called every tick to update the game. Will update towers, minions, etc. */
+/**
+ * Needs to be called every tick to update the game. Will update towers, minions, etc.
+ * */
 class GameManager {
 public:
+    /**
+     * Game Manager
+     */
     GameManager();
 
+    /**
+     * Player's current money
+     */
     int money;
-    Map *map;        /// Pointer to the current game map.
-    void update();    /// This function updates the whole game for one timestep.
+
+    /**
+     * Pointer to the current game map.
+     */
+    Map *map;
+
+    /**
+     * This function updates the whole game for one timestep.
+     */
+    void update();
+
+    /**
+     * Adds a Tower to the Map
+     * @param coordinates where to place the Tower
+     * @param type Type of entity
+     * @return Returns if tower was placed or not according to current Player's money
+     */
     bool addTower(CartesianCoordinates coordinates, ButtonTypes type);
 
+    /**
+     * Player's Minion kills
+     */
     int kills = 0;
+
+    /**
+     * Leve the Player is currently at
+     */
     int level = 0;
+
+    /**
+     * Time between waves
+     */
     int cooldownTime = 0;
+
+    /**
+     * Minion's spawn speed
+     */
     float spawnSpeed = 0.2F;
+
+    /**
+     * All the states of the Game
+     */
     enum state {
         menu, run, pause, lost, won, start, cooldown
-    };        /// Game states
+    };
+
+    /***
+     * State of the Game
+     */
     state gameState = menu;
 
+    /**
+     * Starts a new Game
+     * @param map_path Map path
+     */
     void startNewGame(std::string map_path);
 
+    /**
+     * Resets the Game
+     */
     void resetGame();
 
 private:
-    void shootTowers();            /// This function makes all towers attempt to shoot.
-    void addMinions();            /// This function adds a new minion to the game periodically
-    void moveMinions();            /// This function moves all minions in the right direction.
+    /**
+     * This function makes all towers attempt to shoot.
+     */
+    void shootTowers();
 
-    int ticksToNextMinion = 30;    /// Temporary variable to determine the period of minion spawning.
-    int tickCount = 0;            /// Temporary variable to compare if a new minion should be spawned.
-    int minionsLeftInWave = 0;    /// Number of minions between cooldowns
+    /**
+     * This function adds a new minion to the game periodically
+     */
+    void addMinions();
+
+    /**
+     * This function moves all minions in the right direction.
+     */
+    void moveMinions();
+
+    /**
+     * Temporary variable to determine the period of minion spawning.
+     */
+    int ticksToNextMinion = 30;
+
+    /**
+     * Temporary variable to compare if a new minion should be spawned.
+     */
+    int tickCount = 0;
+
+    /**
+     * Number of minions between cooldowns
+     */
+    int minionsLeftInWave = 0;
 };
 
 
