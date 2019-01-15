e058de12f9c3e40608d7cf89c047c83d64934eca
Gonçalo Camelo Neves Pereira <goncalo_pereira@outlook.pt>
2019-01-15 23:47:59 +0100

    Documentation

--- include/InfoView.h
+++ include/InfoView.h
@@ -4,8 +4,16 @@
 #include "UIElement.h"
 #include "UIText.h"
 
+/**
+ * Displays a panel info information about the game
+ */
 class InfoView : public UIElement {
 public:
+    /**
+     * Information panel
+     * @param quad Rectangular shape of the panel
+     * @param parent Container in which the panel should be placed
+     */
     InfoView(SDL_Rect quad, UIElement *parent);
 
     ~InfoView();
